ba6bf5e2ade28402ee024529bcbbd513fd9c8b02
”guzenco” <>
2022-12-01 23:29:11 +0200

    lab 4 done

--- app/src/main/java/com/lab/ui/main/MainFragment.java
+++ app/src/main/java/com/lab/ui/main/MainFragment.java
@@ -11,13 +11,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.lab.R;
+import com.lab.model.Meteor;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+    private ListView list;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -27,35 +30,32 @@ public class MainFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        mViewModel.setContext(this.getContext());
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View v, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(v, savedInstanceState);
+        list = v.findViewById(R.id.list);
+        mViewModel.getMeteors().observe(getViewLifecycleOwner(), this::onMeteorsChanged);
+    }
 
-        Button start_bt = v.findViewById(R.id.start_btn);
-
-        start_bt.setOnClickListener(view -> {
+    private void onMeteorsChanged(Meteor meteors[]) {
+        if(meteors == null) return;
+        ArrayAdapter<Meteor> adapter = new ArrayAdapter<Meteor>(this.getContext(), android.R.layout.simple_list_item_1, meteors);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener((adapterView, view, i, l) ->
+        {
             getFragmentManager().beginTransaction()
                     .addToBackStack(null)
-                    .replace(R.id.container, GameFragment.newInstance())
-                    .commit();
-        });
-
-        Button stngs_bt = v.findViewById(R.id.settings_btn);
-
-        stngs_bt.setOnClickListener(view -> {
-            getFragmentManager().beginTransaction()
-                    .addToBackStack(null)
-                    .replace(R.id.container, SettingsFragment.newInstance())
+                    .replace(R.id.container, MeteorInfoFragment.newInstance(meteors[i]))
                     .commit();
         });
     }
